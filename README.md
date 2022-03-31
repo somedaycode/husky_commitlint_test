cc84febd61839e5621e139897dcd9c152e625141
Seonkyu Kim <somedaycode@gmail.com>
2022-03-31 17:30:57 +0900

    test: [frontend] apply custom commitlint rule

--- README.md
+++ README.md
@@ -97,6 +97,25 @@ module.exports = {
 
 ```js
 module.exports = {
-  
-}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'develop-rule': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'develop-rule': ({subject}) => {
+          const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
+          return [
+            commitFolders.some((folder) => folder === subject),
+            `\n${commitFolders.map(folder => `${folder}\n`).join('')}
+위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
+ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`
+          ]
+        },
+      }
+    }
+  ]
+};
+
 ```
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'develop-rule': ({subject}) => {
-      const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
-      return [
-        commitFolders.some((folder) => folder === subject),
-        `${commitFolders.forEach(folder => console.log(`${folder}\n`))}
-        위 네 가지가 콜론 뒤에 포함되어야 합니다.
-        `
-      ]
+    'develop-rule': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'develop-rule': ({subject}) => {
+          const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
+          return [
+            commitFolders.some((folder) => folder === subject),
+            `\n${commitFolders.map(folder => `${folder}\n`).join('')}
+위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
+ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`
+          ]
+        },
+      }
     }
-  }
+  ]
 };
