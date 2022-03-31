fa7c418e98f1429c93c26cc5a35ed8db2a695dfd
Seonkyu Kim <somedaycode@gmail.com>
2022-03-31 17:53:21 +0900

    fix: [root] develop-rule * 콜론 뒤에 스페이스바 없이 바로 subject을 붙이면 subject이 null로 나와서 문제가 발생 * 조건식 추가 * 옵셔널을 추가해봐야겠다.

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,13 +9,13 @@ module.exports = {
         'develop-rule': ({subject}) => {
           const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
           return [
-            commitFolders.some((folder) => subject.startsWith(folder)),
+            subject ? commitFolders.some((folder) => subject.startsWith(folder)) : false,
             `\n${commitFolders.map(folder => `${folder}\n`).join('')}
 위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
 ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`
           ]
         },
-      }
+      },
     }
   ]
 };
