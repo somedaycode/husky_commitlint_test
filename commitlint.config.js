b0293af2c28781c85080bd45650c78398617aecb
Seonkyu Kim <somedaycode@gmail.com>
2022-03-31 17:54:48 +0900

    fix: [root] develop-rule 함수 로직

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'develop-rule': ({subject}) => {
           const commitFolders = ['[frontend]', '[backend]', '[domain]', '[root]'];
           return [
-            subject ? commitFolders.some((folder) => subject.startsWith(folder)) : false,
+            commitFolders.some((folder) => subject?.startsWith(folder)),
             `\n${commitFolders.map(folder => `${folder}\n`).join('')}
 위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
 ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`
